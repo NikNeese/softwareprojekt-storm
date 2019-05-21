d161255ab69ca211348b1fcdac53ae97cc655ec9
Niklas Neesemann <>
2019-05-21 21:43:12 +0200

    prepare.sh added

---
+++ prepare.sh
@@ -0,0 +1,15 @@
+mkdir -p /srv/{formulas,pillar,salt}
+cd /srv/formulas
+cat <<EOT >> master
+file_roots:
+  base:
+    - /srv/salt/
+    - /srv/formulas
+    - /srv/formulas/zookeeper-formula
+    - /srv/formulas/salt-formula-storm-1
+pillar_roots:
+  base:
+    - /srv/pillar
+EOT
+sudo rm /etc/salt/master
+sudo mv master /etc/salt/
